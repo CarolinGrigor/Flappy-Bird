58455899a40bffc652ce656791df939140a6a459
CarolinGrigor <125268417+CarolinGrigor@users.noreply.github.com>
2023-07-19 23:53:20 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Happy_Bird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.scoreText = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ostacolobasso = new System.Windows.Forms.PictureBox();
+            this.ostacoloalto = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.terra = new System.Windows.Forms.PictureBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            ((System.ComponentModel.ISupportInitialize)(this.ostacolobasso)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ostacoloalto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.terra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.BackColor = System.Drawing.Color.Transparent;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(25, 58);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(0, 20);
+            this.scoreText.TabIndex = 4;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timerevent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 37);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ostacolobasso
+            // 
+            this.ostacolobasso.BackColor = System.Drawing.Color.Transparent;
+            this.ostacolobasso.Image = ((System.Drawing.Image)(resources.GetObject("ostacolobasso.Image")));
+            this.ostacolobasso.Location = new System.Drawing.Point(347, 270);
+            this.ostacolobasso.Name = "ostacolobasso";
+            this.ostacolobasso.Size = new System.Drawing.Size(66, 386);
+            this.ostacolobasso.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ostacolobasso.TabIndex = 2;
+            this.ostacolobasso.TabStop = false;
+            // 
+            // ostacoloalto
+            // 
+            this.ostacoloalto.BackColor = System.Drawing.Color.Transparent;
+            this.ostacoloalto.Image = ((System.Drawing.Image)(resources.GetObject("ostacoloalto.Image")));
+            this.ostacoloalto.Location = new System.Drawing.Point(347, -205);
+            this.ostacoloalto.Name = "ostacoloalto";
+            this.ostacoloalto.Size = new System.Drawing.Size(66, 386);
+            this.ostacoloalto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ostacoloalto.TabIndex = 1;
+            this.ostacoloalto.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.Color.Transparent;
+            this.bird.Image = global::Happy_Bird.Properties.Resources.birdup_removebg_preview;
+            this.bird.Location = new System.Drawing.Point(12, 209);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(46, 38);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // terra
+            // 
+            this.terra.Image = ((System.Drawing.Image)(resources.GetObject("terra.Image")));
+            this.terra.Location = new System.Drawing.Point(-1, 452);
+            this.terra.Name = "terra";
+            this.terra.Size = new System.Drawing.Size(1684, 50);
+            this.terra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.terra.TabIndex = 3;
+            this.terra.TabStop = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 501);
+            this.splitter1.TabIndex = 7;
+            this.splitter1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Happy_Bird.Properties.Resources.download;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(509, 501);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.terra);
+            this.Controls.Add(this.ostacolobasso);
+            this.Controls.Add(this.ostacoloalto);
+            this.Controls.Add(this.bird);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyup);
+            ((System.ComponentModel.ISupportInitialize)(this.ostacolobasso)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ostacoloalto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.terra)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox ostacoloalto;
+        private System.Windows.Forms.PictureBox ostacolobasso;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox terra;
+        private System.Windows.Forms.Splitter splitter1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Happy_Bird
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player;
+        int velocita = 6;
+        int gravita = 5;
+        int score = 0;
+        bool gameover = false;
+        int pos, topb;
+        Random random = new Random();
+        Random rb = new Random();
+        Random nd = new Random();
+        Button retryButton;
+        Button startButton;
+        DateTime startTime;
+        Stopwatch stopwatch = new Stopwatch();
+        double targetFrameTime = 1000.0 / 120.0; // Target tempo di frame (16.67ms per 120 FPS)
+    
+        public Form1()
+        {
+            InitializeComponent();
+
+            retryButton = new Button();
+            retryButton.Text = "RETRY";
+            retryButton.Size = new Size(80, 40);
+            retryButton.Location = new Point(200, 200);
+            retryButton.Visible = false;
+            retryButton.Enabled = false;
+            retryButton.BackColor = Color.MintCream;
+            retryButton.Click += retryButton_Click;
+            Controls.Add(retryButton);
+
+            startButton = new Button();
+            startButton.Text = "START";
+            startButton.Size = new Size(80, 40);
+            startButton.Location = new Point(200, 300);
+            startButton.Click += startButton_Click;
+            startButton.BackColor = Color.LightCyan;
+            Controls.Add(startButton);
+
+            /*retryButton.Click rappresenta l'evento di click del pulsante startButton, 
+            mentre retryButton_Click rappresenta il metodo che viene eseguito quando
+            si verifica l'evento di click*/
+
+            /*Controls.Add(retryButton) serve ad aggiungere il pulsante retryButton alla
+            raccolta di controlli del form, consentendo di visualizzarlo e interagire con 
+            esso all'interno dell'applicazione*/
+
+            /*startButton.Click rappresenta l'evento di click del pulsante startButton,
+            mentre startButton_Click rappresenta il metodo che viene eseguito quando
+            si verifica l'evento di click*/
+
+            /*Controls.Add(startButton) serve ad aggiungere il pulsante startButton alla
+            raccolta di controlli del form, consentendo di visualizzarlo e interagire con 
+            esso all'interno dell'applicazione*/
+
+            bird.Visible = false;
+            ostacoloalto.Visible = false;
+            ostacolobasso.Visible = false;
+            terra.Visible = false;
+            scoreText.Visible = false;
+            label1.Location = new Point(90, 200);
+            label1.Font = new Font(label1.Font.FontFamily, 42, FontStyle.Bold);
+            label1.Text = "";
+            this.BackgroundImage = Happy_Bird.Properties.Resources.immaginestart;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            timer.Stop(); // Ferma il timer all'avvio del gioco
+
+            player = new SoundPlayer(@"C:\Users\Carolin\Downloads\Music (online-audio-converter.com).wav");
+            player.PlayLooping();
+
+
+            this.DoubleBuffered = true;
+        }
+
+        public void timerevent(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+            timer1.Start();
+            bird.Visible = true;
+            ostacoloalto.Visible = true;
+            ostacolobasso.Visible = true;
+            terra.Visible = true;
+            scoreText.Visible = true;
+            label1.Visible = false;
+            this.BackgroundImage = Happy_Bird.Properties.Resources.download;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+
+            if (!gameover)
+            {
+                bird.Top += gravita;
+                ostacoloalto.Left -= velocita;
+                ostacolobasso.Left -= velocita;
+
+                scoreText.Text = score.ToString();
+                scoreText.Text = "Score: " + score;
+
+                pos = random.Next(47, 70);
+                topb = rb.Next(112, 417);
+
+                if (ostacolobasso.Left < -pos)
+                {
+                    ostacolobasso.Left = 497;
+                    ostacolobasso.Top = topb;
+                    score++;
+                }
+
+                if (ostacoloalto.Left < -pos)
+                {
+                    ostacoloalto.Left = 497;
+                    ostacoloalto.Top = topb - 475;
+                    score++;
+                }
+
+                if (bird.Bounds.IntersectsWith(ostacoloalto.Bounds) ||
+                    bird.Bounds.IntersectsWith(ostacolobasso.Bounds) ||
+                    bird.Bounds.IntersectsWith(terra.Bounds))
+                {
+                    Finish();
+                    retryButton.Visible = true;
+                    retryButton.Enabled = true;
+                }
+
+                if (score > 10)
+                {
+                    velocita = 10;
+                }
+
+                if (score > 20)
+                {
+                    velocita = 13;
+                }
+
+                if (score > 30)
+                {
+                    velocita = 20;
+                }
+
+                if (bird.Top < -20)
+                {
+                    Finish();
+                    retryButton.Visible = true;
+                    retryButton.Enabled = true;
+                }
+            }
+
+            stopwatch.Stop();
+
+            // Controlla il tempo trascorso per il frame e attendi se necessario per raggiungere il target frame rate
+            double elapsedMilliseconds = stopwatch.Elapsed.TotalMilliseconds;
+            double remainingTime = targetFrameTime - elapsedMilliseconds;
+
+            if (remainingTime > 0)
+                Thread.Sleep((int)remainingTime);
+
+            stopwatch.Reset();
+            Invalidate(); // Forza il ridisegno del form
+        }
+
+        private void keydown(object sender, KeyEventArgs e)
+        {
+            if (!gameover && e.KeyCode == Keys.Space)
+            {
+                e.Handled = true; // Impedisce la riproduzione dell'audio di errore
+                e.SuppressKeyPress = true; // Impedisce la propagazione del tasto Space
+                bird.Visible = true;
+                bird.Image = Happy_Bird.Properties.Resources.birdup_removebg_preview;
+                gravita = -5;
+            }
+        }
+
+        private void keyup(object sender, KeyEventArgs e)
+        {
+            if (!gameover && e.KeyCode == Keys.Space)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                bird.Visible = true;
+                bird.Image = Happy_Bird.Properties.Resources.birddown_removebg_preview;
+                gravita = 5;
+            }
+        }
+
+        private void Finish()
+        {
+            timer.Stop();
+            bird.Visible = false;
+            ostacoloalto.Visible = false;
+            ostacolobasso.Visible = false;
+            terra.Visible = false;
+            scoreText.Visible = false;
+            label1.Visible = true;
+            label1.Location = new Point(130, 154);
+            label1.Font = new Font(label1.Font.FontFamily, 12, FontStyle.Bold);
+            label1.BackColor = Color.Transparent;
+            label1.Text = "Game Over! Your score is: " + score;
+            this.BackgroundImage = Happy_Bird.Properties.Resources.download;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            gameover = true;
+        }
+
+        private void restart()
+        {
+            gameover = false;
+            bird.Location = new Point(12, 209);
+
+            bird.Visible = false;
+            ostacolobasso.Location = new Point(347, 270);
+            ostacoloalto.Location = new Point(347, -205);
+            ostacoloalto.Visible = false;
+            ostacolobasso.Visible = false;
+            terra.Visible = false;
+            scoreText.Visible = false;
+            label1.Location = new Point(90, 200);
+            label1.Font = new Font(label1.Font.FontFamily, 42, FontStyle.Bold);
+            label1.Text = "";
+            this.BackgroundImage = Happy_Bird.Properties.Resources.immaginestart;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            label2.Visible = false;
+
+            velocita = 6;
+            score = 0;
+
+            ostacoloalto.Left = 347;
+            ostacolobasso.Left = 347;
+
+            retryButton.Visible = false;
+            retryButton.Enabled = false;
+
+            startButton.Visible = true; // Mostra il pulsante di avvio
+            startButton.Enabled = true; // Abilita l'interazione con il pulsante di avvio
+        }
+
+        private void retryButton_Click(object sender, EventArgs e)
+        {
+            restart();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (gameover)
+            {
+                label2.Text = "";
+            }
+            else
+            {
+                TimeSpan elapsedTime = DateTime.Now - startTime;
+                label2.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            startButton.Enabled = false;
+            label2.Visible = true;
+            timer.Start(); 
+            startTime = DateTime.Now; // Memorizza il tempo di inizio del gioco
+        }
+    }
+}
